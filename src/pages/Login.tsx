2bd34879b44340b48ee312494356ea99952907a5
Dsovichy <Dauschurchill@gmail.com>
2025-02-13 14:19:04 +0800

    Fixed errors on login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,36 +1,35 @@
 import { 
   IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
-    const doLogin = () => {
+  IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar, 
+    useIonRouter
+} from '@ionic/react';
+
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
+
+  const doLogin = () => {
       navigation.push('/it35-lab/app','forward','replace');
   }
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-        <IonButton onClick={() => doLogin()} expand="full">
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Login;
-  
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding'>
+          <IonButton onClick={() => doLogin()} expand="full">
+              Login
+          </IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
