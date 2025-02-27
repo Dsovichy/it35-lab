299ec6704d11424becd6f319554bc87fed003ad5
Dsovichy <Dauschurchill@gmail.com>
2025-02-27 09:04:55 +0800

    Updates

--- assets/ios.transition-legacy-CvJGmX6K.js
+++ assets/ios.transition-legacy-CvJGmX6K.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CWACsR9I.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-zMU0YT5T.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-N8QqRIi_.js
+++ assets/keyboard2-legacy-N8QqRIi_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CWACsR9I.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-zMU0YT5T.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
