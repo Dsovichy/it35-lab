b9d167c82f6898661ee06ff817ced33a00f1e435
Dsovichy <Dauschurchill@gmail.com>
2025-02-27 08:58:15 +0800

    Updates

--- assets/swipe-back-legacy-C8fFYo_q.js
+++ assets/swipe-back-legacy-C8fFYo_q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BLrhNQew.js","./index3-legacy-DdT0fioQ.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CWACsR9I.js","./index3-legacy-D8jYhmKc.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
