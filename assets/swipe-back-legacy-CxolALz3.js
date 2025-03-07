7bddcea5d12e25d39c3908407748f89f2bae6c0d
Dsovichy <143605437+Dsovichy@users.noreply.github.com>
2025-03-07 16:00:37 +0800

    Updates

--- assets/swipe-back-legacy-CxolALz3.js
+++ assets/swipe-back-legacy-CxolALz3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-zMU0YT5T.js","./index3-legacy-DvAsqExM.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-C5BBvNEW.js","./index3-legacy-BxMMugh0.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
