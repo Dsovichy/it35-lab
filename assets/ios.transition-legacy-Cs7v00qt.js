3e195d22ea52314bfcc4f5cccb554ae62256858c
Dsovichy <143605437+Dsovichy@users.noreply.github.com>
2025-03-18 20:02:38 +0800

    Updates

--- assets/ios.transition-legacy-Cs7v00qt.js
+++ assets/ios.transition-legacy-Cs7v00qt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CYSxUKXY.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-FL3dDtdu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-B6EGRyJz.js
+++ assets/md.transition-legacy-B6EGRyJz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CYSxUKXY.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-FL3dDtdu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
