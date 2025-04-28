3e9434f7a5dc29912617f2cfe803e5b282a3040f
Dsovichy <Dauschurchill@gmail.com>
2025-04-28 08:46:17 +0800

    Updates

--- assets/md.transition-legacy-BDxO9Zeo.js
+++ assets/md.transition-legacy-BDxO9Zeo.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BY8qfYVP.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-GZSTq3p1.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
