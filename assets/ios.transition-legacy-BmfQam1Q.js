c04d4b8bef0a3d0578fc929cc27417a0ef3514ed
Dsovichy <Dauschurchill@gmail.com>
2025-05-05 15:34:08 +0800

    Updates

--- assets/ios.transition-legacy-BmfQam1Q.js
+++ assets/ios.transition-legacy-BmfQam1Q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DGesgA0R.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DGT2Kycf.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
